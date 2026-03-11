f404ea6949be99d157d3933ca91770b1b22f7919
malekmezidi-cpu <malek.mezidi@remax-quebec.com>
2026-03-11 01:22:25 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Script from "next/script";
 
 export const metadata = {
   title: "Préavis 60 jours Montréal | Vente rapide maison | ProprioSécur",
@@ -15,19 +14,35 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-HYN7QWDCE2"
-          strategy="afterInteractive"
+
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HYN7QWDCE2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HYN7QWDCE2');
+            `,
+          }}
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-HYN7QWDCE2');
-          `}
-        </Script>
+
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/vtxj8fo17a";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vtxj8fo17a");
+            `,
+          }}
+        />
+
       </head>
+
       <body>{children}</body>
     </html>
   );
